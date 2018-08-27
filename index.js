9f562a89c362d488d0682c3f9539ecedd336270f
Monox <ghofarsuprapto@gmail.com>
2018-08-27 11:04:58 +0700

    Fix

--- index.js
+++ index.js
@@ -8,24 +8,32 @@ const {URL} = require('url');
 const fetch = require('node-fetch');
 const api = require('./api.json');
 
-module.exports = function RexTester(code, options) {
+const RexTester = function (code, options) {
     if (!RexTester.Promise) {
         throw new Error('Native promise is missing, you can set it by do RexTester.Promise = (Your Promise)');
     }
     
     if (!options) options = { language: 'nodejs' };
     
+    const fetchOptions = {
+        "method": "POST",
+        "qs": {
+            "LanguageChoice": api.includes(options.language.toLowerCase()) ? api[options.language.toLowerCase()] : null;
+            "Program": typeof code === 'string' || code.length > 0 ? code : '';
+        }
+    }
+   
+    if (options.args) fetchOptions.qs['CompilerArgs'] = options.args
+    
     return new RexTester.Promise(async (resolve, reject) => {
-        const body = await fetch('http://rextester.com/rundotnet/api', {
-            "method": "POST",
-            "qs": {
-                "LanguageChoice": api.includes(options.language.toLowerCase()) ? api[options.language.toLowerCase()] : null;
-                "Program": typeof code === 'string' || code.length > 0 ? code : '';
-            }
-        });
+        const body = await fetch('http://rextester.com/rundotnet/api', fetchOptions);
         
         const json = await body.json();
         
-        return 
+        return { status: json.Errors ? "error" : "ok", result: json.Errors ? json.Errors : json.Result };
     })
-}  
+}
+
+RexTester.Promise = Promise;
+
+module.exports = RexTester;
